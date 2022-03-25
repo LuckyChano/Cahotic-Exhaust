35b630d9b9d7827ef5cc7fb1b0b6415d43ee65d7
DESKTOP-R7MU1UI\NicoR <nicor_reale@hotmail.com>
2022-03-25 11:51:15 -0300

    Agregado movimiento basico y camara con efecto de head bobbing. Todo en una escena nueva llamada Test Scene para probar las cosas

---
+++ Chaotic Exhaust/Assets/CamMovement.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CamMovement : MonoBehaviour
+{
+
+    public float mouseSens = 100f;
+
+    public Transform playerBody;
+
+    private CharMovement _player;
+
+    float YmovSens = 0.08f
+        , YRange = 0.15f
+        , YlocalPos;
+
+    float xRotation = 0f;
+
+    float ellapsedTime;
+
+
+    private void Start()
+    {
+        YlocalPos = transform.localPosition.y;
+
+        _player = GetComponentInParent<CharMovement>();
+    }
+    protected private void Update()
+    {
+        if(_player._canMove)
+        {
+            float mouseX = Input.GetAxis("Mouse X") * mouseSens * Time.deltaTime;
+            float mouseY = Input.GetAxis("Mouse Y") * mouseSens * Time.deltaTime;
+
+            playerBody.Rotate(Vector3.up * mouseX);
+
+            xRotation -= mouseY;
+
+            xRotation = Mathf.Clamp(xRotation, -90, 90);
+
+            transform.localRotation = Quaternion.Euler(xRotation, 0, 0);
+            if (_player._isMoving && !_player._isJumping)
+            {
+
+                CamSineWave(YRange, YmovSens, YlocalPos);
+            }
+        }
+    }
+
+    public void CamSineWave(float amp, float b, float k)
+    {
+        if(ellapsedTime >= 1000)
+        {
+            ellapsedTime = 0;
+        }
+        float YPos = amp * Mathf.Sin((ellapsedTime - 1) / b) + k;
+        ellapsedTime += Time.deltaTime;
+        transform.localPosition = new Vector3(transform.localPosition.x, YPos, transform.localPosition.z);
+
+    }
+}
---
+++ Chaotic Exhaust/Assets/CharMovement.cs
@@ -0,0 +1,91 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharMovement : MonoBehaviour, IWalkable
+{
+    protected float speed = 5f;
+
+    protected float jumpHeight = 300f;
+
+    protected Vector3 position;
+
+    protected float verAxis;
+
+    protected float horAxis;
+
+    protected Rigidbody _rb;
+
+    public float raydistance = 0f;
+
+    public LayerMask layerMask;
+
+    //[HideInInspector]
+    public bool _isMoving, _isJumping, _canMove;
+
+
+    private void Start()
+    {
+        _canMove = true;
+        _rb = GetComponent<Rigidbody>();
+        _isMoving = false;
+    }
+
+    public void Move()
+    {
+            if (Input.GetKey(KeyCode.LeftShift))
+            {
+                speed = 10;
+            }
+            else speed = 5f;
+            position = transform.position;
+
+            position += (speed * verAxis * Time.deltaTime) * transform.forward;
+
+            if (horAxis != 0)
+            {
+                position += (speed * horAxis * Time.deltaTime) * transform.right;
+            }
+
+            _rb.MovePosition(position);
+
+
+            if (verAxis != 0 || horAxis != 0)
+            {
+                _isMoving = true;
+            }
+            else _isMoving = false;
+    }
+    public void Jump()
+    {
+        _rb.AddForce(Vector3.up * jumpHeight);
+    }
+
+    void JumpRay()
+    {
+        
+        if(Physics.Raycast(transform.position, transform.TransformDirection(Vector3.down), raydistance, layerMask))
+        {          
+            _isJumping = false;
+        }
+        else
+        {
+            _isJumping = true;
+        }
+    }
+
+    private void Update()
+    {
+        JumpRay();
+        if (Input.GetKeyDown(KeyCode.Space) && !_isJumping)
+            {
+                Jump();
+            }
+    }
+    virtual protected void FixedUpdate()
+    {
+        verAxis = Input.GetAxis("Vertical");
+        horAxis = Input.GetAxis("Horizontal");
+        Move();
+    }
+}
