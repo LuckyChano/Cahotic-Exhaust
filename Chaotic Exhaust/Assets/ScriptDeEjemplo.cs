1737ce895231d5588354027b1d17a571bb723149
LuckyChano <101567723+LuckyChano@users.noreply.github.com>
2022-05-08 03:53:33 -0300

    v0.76 - Script de Ejemplo

---
+++ Chaotic Exhaust/Assets/ScriptDeEjemplo.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScriptDeEjemplo : MonoBehaviour
+{
+    //Variables Estaticas
+
+    //Variables Publicas por Referencia
+
+    //Variables Privadas por Referencia
+
+    //Variables Publicas
+
+    //Variables Privadas
+
+    //Propiedades
+
+    /*  Metodos de Unity en este orden
+            - Awake
+            - Start
+            - Updates
+            - Collisions
+    */
+
+    //Nuestros Metodos Privados
+
+    //Nuestros Metodos Publicos
+
+    //Nuestros Metodos Heredados que comienzan con virtual
+
+    //Nuestros Metodos Herdados y modificados que comienzan con override
+
+    //Cosas raras que no sabemos usar XD..
+}
