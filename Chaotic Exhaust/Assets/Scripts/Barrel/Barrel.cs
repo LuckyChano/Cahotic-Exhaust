4056f668cda80aa6e1bea9e99f7e2941b9e06127
JANONIMO <JulianDiegoBenitez@gmail.com>
2022-05-12 17:03:20 -0300

    Mini cambio v0.96

--- Chaotic Exhaust/Assets/Scripts/Barrel/Barrel.cs
+++ Chaotic Exhaust/Assets/Scripts/Barrel/Barrel.cs
@@ -4,25 +4,25 @@ using UnityEngine;
 public class Barrel : MonoBehaviour, IsShootable
 {
     private RoomTrigger _roomTrigger;
-    AudioSource hitShot;
+    AudioSource _hitShot;
     //public int hp = 1;
-    LifeBehaviour hpComponent;
+    LifeBehaviour _hpComponent;
     public int hp;
     
     private void Awake()
     {
-        hitShot = GetComponent<AudioSource>();
+        _hitShot = GetComponent<AudioSource>();
         _roomTrigger = FindObjectOfType<RoomTrigger>();
     }
     
     void Start()
     {
-        hpComponent = new LifeBehaviour(DamageFeedback, hitShot, hp);
+        _hpComponent = new LifeBehaviour(DamageFeedback, _hitShot, hp);
     }
     
     public void Damage(int dmg)
     {
-        hpComponent.takeDamage(dmg);
+        _hpComponent.takeDamage(dmg);
     }
 
     public void DamageFeedback()
@@ -38,7 +38,7 @@ public class Barrel : MonoBehaviour, IsShootable
         yield return new WaitForSeconds(.5f);
 
         GetComponentInChildren<Renderer>().material.color = Color.white;
-        if (hpComponent.hp <= 0)
+        if (_hpComponent.hp <= 0)
         { 
             Destroy(gameObject);
         }
@@ -47,6 +47,6 @@ public class Barrel : MonoBehaviour, IsShootable
     
     public int ReturnHP()
     {
-        return hpComponent.hp;
+        return _hpComponent.hp;
     }
 }
