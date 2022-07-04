9beedd1c932ac0f5b82f35a0101d45ac03c28cd4
IPiperI <piper_liuterman@outlook.com>
2022-07-04 15:46:28 -0300

    AudioManager :+1:

--- Chaotic Exhaust/Assets/AudioManager.cs
+++ Chaotic Exhaust/Assets/AudioManager.cs
@@ -4,9 +4,25 @@ using System;
 
 public class AudioManager : MonoBehaviour
 {
+    public static AudioManager instance;
+
     public Sound[] sounds;
+
     void Awake()
     {
+
+        if (instance == null) 
+        {
+            instance = this;
+        }     
+        else
+        {
+            Destroy(gameObject);
+        }
+
+        DontDestroyOnLoad(gameObject);
+
+
         foreach (Sound s in sounds)
         {
             s.source = gameObject.AddComponent<AudioSource>();
@@ -15,12 +31,21 @@ public class AudioManager : MonoBehaviour
 
             s.source.pitch = s.pitch;
             s.source.volume = s.volume;
+            s.source.loop = s.loop;
         }
     }
+
+    //private void Start()
+    //{
+    //    Play("Theme");
+    //}
 
     public void Play (string name)
     {
         Sound s = Array.Find(sounds, sound => sound.name == name);
+        if (s == null)
+           return;       
+       
         s.source.Play();
 
     }
--- Chaotic Exhaust/Assets/Sound.cs
+++ Chaotic Exhaust/Assets/Sound.cs
@@ -12,4 +12,11 @@ public class Sound
     public float pitch;
     [Range(0f, 1f)]
     public float volume;
+
+    public bool loop;
+
+
+    [HideInInspector] public AudioSource source;
+
+
 }
